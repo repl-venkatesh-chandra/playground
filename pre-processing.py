842c8b14a8f3e678c7ffebc7a5c2282289cc56f9
repl-venkatesh-chandra <67432477+repl-venkatesh-chandra@users.noreply.github.com>
2020-11-10 15:06:27 -0700

    Update pre-processing.py

--- pre-processing.py
+++ pre-processing.py
@@ -4,7 +4,7 @@
 """
 
 import pandas
-from sklearn.preprocessing import StandardScaler
+
 
 risk_df = pandas.read_csv("data_raw.csv")
 
@@ -12,11 +12,6 @@ risk_df = pandas.read_csv("data_raw.csv")
 X = risk_df.iloc[:,2:5]
 y = risk_df['risk']
 
-#Standardize the columns
-
-standardizer = StandardScaler()
-X_std = standardizer.fit_transform(X)
-
 #Save output
-X_std.to_csv("X.csv")
+X.to_csv("X.csv")
 y.to_csv("y.csv")
