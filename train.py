5517ded6137f614be030e7c4b3196da820a9af4d
Venkatesh Chandra <venkatesh.chandra@replicon.com>
2020-11-12 10:52:21 -0700

    updated

--- train.py
+++ train.py
@@ -43,8 +43,8 @@ with open("metrics.txt", 'w') as outfile:
     outfile.write("Recall %5.3f%%\n" % recall)
 	
 #Save parameters
-with open("parameters.txt", 'w') as outfile:
-    outfile.write("k = 4")
+with open("params.txt", 'w') as outfile2:
+    outfile2.write("k = 4")
 
 #Metrics diff
 
